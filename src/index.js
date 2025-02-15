ac41a2867a9237915d55e5fddf71a1f9fbac439a
Радіон <radion.zorkin@gmail.com>
2025-02-15 14:46:57 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,82 @@
+const tools = [
+  {
+    id: 1,
+    img: 'https://static.dnipro-m.ua/cache/products/6566/catalog_origin_270253.jpg',
+    name: 'Тример',
+    price: 3699,
+    description:
+      'Тример електричний Dnipro-M 110 призначений для покосу густої трави, а також чагарників.',
+  },
+  {
+    id: 2,
+    img: 'https://static.dnipro-m.ua/cache/products/6483/catalog_origin_270227.jpg',
+    name: 'Мотокоса',
+    price: 11049,
+    description:
+      "Мотокоса Dnipro-M 43 призначена для покосу трави, чагарників, бур'янів, газонів.",
+  },
+  {
+    id: 3,
+    img: 'https://static.dnipro-m.ua/cache/products/2741/catalog_origin_271775.jpg',
+    name: 'Генератор',
+    price: 10890,
+    description:
+      'Бензиновий генератор GX-25 номінальною потужністю 2,5 кВт забезпечить автономне живлення.',
+  },
+  {
+    id: 4,
+    img: 'https://static.dnipro-m.ua/cache/products/7546/catalog_origin_275351.jpg',
+    name: 'Перфоратор',
+    price: 4899,
+    description:
+      'Перфоратор Dnipro-M підходить для свердління та довбання бетону, каменю і цегли.',
+  },
+  {
+    id: 5,
+    img: 'https://static.dnipro-m.ua/cache/products/5723/catalog_origin_270653.jpg',
+    name: 'Шуруповерт',
+    price: 2599,
+    description:
+      'Шуруповерт Dnipro-M допоможе у виконанні складних завдань із закручування та свердління.',
+  },
+  {
+    id: 6,
+    img: 'https://static.dnipro-m.ua/cache/products/6233/catalog_origin_271127.jpg',
+    name: 'Кутова шліфмашина',
+    price: 3299,
+    description:
+      'Кутова шліфувальна машина Dnipro-M для обробки металу, бетону та дерева.',
+  },
+  {
+    id: 7,
+    img: 'https://static.dnipro-m.ua/cache/products/8135/catalog_origin_272441.jpg',
+    name: 'Пила ланцюгова',
+    price: 7999,
+    description:
+      'Електрична ланцюгова пила Dnipro-M призначена для швидкого розпилювання деревини.',
+  },
+  {
+    id: 8,
+    img: 'https://static.dnipro-m.ua/cache/products/4012/catalog_origin_270012.jpg',
+    name: 'Відбійний молоток',
+    price: 14500,
+    description:
+      'Відбійний молоток Dnipro-M для демонтажу бетону та інших міцних матеріалів.',
+  },
+  {
+    id: 9,
+    img: 'https://static.dnipro-m.ua/cache/products/4534/catalog_origin_271352.jpg',
+    name: 'Зварювальний апарат',
+    price: 6299,
+    description:
+      'Зварювальний апарат Dnipro-M для якісного зварювання металів.',
+  },
+  {
+    id: 10,
+    img: 'https://static.dnipro-m.ua/cache/products/5132/catalog_origin_271654.jpg',
+    name: 'Електролобзик',
+    price: 1999,
+    description:
+      'Електролобзик Dnipro-M для виконання точних розпилів у дереві та металі.',
+  },
+];
